a6a221acb9f3615ae8c9390e05b5e50f22f368e9
VitaminB16 <artemiy.nosov@gmail.com>
2024-06-10 18:21:01 +0100

    update: files

--- src/copy_files.sh
+++ src/copy_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Copy VSCode settings.json
 sudo cp ~/Library/'Application Support'/Code/User/settings.json files/VSCode/settings.json
-# Copy the .bash_profile file to files/system/.bash_profile
+# Copy .bash_profile
 sudo cp ~/.bash_profile files/system/.bash_profile
