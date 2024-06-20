f824819bd86426fbe9df2ccd7fe87f4e4e61d3c3
VitaminB16 <artemiy.nosov@gmail.com>
2024-06-20 20:16:56 +0100

    feat: 'to'/'from' arguments

--- main.sh
+++ main.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-chmod +x src/copy_files.sh
-sudo src/copy_files.sh
-python src/set_bash_profile.py
+arg=$1
+
+if [ "$arg" == "from" ]; then
+    # ~/.bash_profile -> files/system/.bash_profile, etc.
+    chmod +x src/copy_files.sh
+    sudo src/copy_files.sh
+elif [ "$arg" == "to" ]; then
+    # files/system/.bash_profile -> ~/.bash_profile, etc.
+    python src/set_bash_profile.py
+else
+    echo "Invalid argument"
+    exit 1
+fi
